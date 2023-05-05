576fae71827ea963f5fdac1e9dbedb292f3eeda8
PieVieRo <tymek24m@gmail.com>
2023-05-05 15:49:34 +0200

    jestesmy w domu

--- flapujacy-igor/main.cpp
+++ flapujacy-igor/main.cpp
@@ -14,10 +14,16 @@ int najwyzszy = 0;
 
 int main(int argc, char* argv[]) {
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, u8"FLAPUJ¥CY IGOR");
+	Image dom = LoadImage("images/dom.jpg");
+	ImageResize(&dom, SCREEN_WIDTH, SCREEN_HEIGHT);
+	Texture2D domTexture = LoadTextureFromImage(dom);
 	
 	while (!WindowShouldClose()) {
 		BeginDrawing();
+
 		ClearBackground(WHITE);
+		DrawTexture(domTexture, 0, 0, WHITE);
+
 		EndDrawing();
 	}
 	CloseWindow();
